47adad4bb48eeb8d5455c8b606806cd3ad683504
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-09-02 18:19:47 +0600

    First batch of changes

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task3;
+import java.lang.*;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!((character >= 'A'  && character <= 'Z') || (character >= 'a' && character <= 'z')))
+            System.out.println("wrong alphabet!");
+
+        character = Character.toLowerCase(character);
+        if (character == 'a' || character == 'e' || character == 'i' ||
+                character == 'u' || character == 'o')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
